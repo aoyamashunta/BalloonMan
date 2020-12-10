d6a5affb2a668a190de08176ecc83668fa0c4da3
aoyamashunta <k020g1114@g.neec.ac.jp>
2020-12-10 21:48:53 +0900

    Inputクラス追加

---
+++ BalloonMan/Input.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "DxLib.h"
+
+class Input
+{
+private:
+	static char keys[256];
+	static char oldkeys[256];
+
+public:
+	static void Update();
+
+	static bool isKey(int key);
+
+	static bool isKeyTrigger(int key);
+};
